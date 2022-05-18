cd4f17e4f73497233a04e63237b1b2a7afa7f007
savneetkaur21 <savneet@tru.agency>
2022-05-18 22:00:44 +0530

    all reports' fields

--- libs/core/src/component/store/store.service.ts
+++ libs/core/src/component/store/store.service.ts
@@ -1093,13 +1093,125 @@ export class StoreService {
             },
           )
           .pipe(
-            map((response) => {
+            map(async (response) => {
               if (response.data && response.data.length) {
                 page += 1;
+                let data = response.data;
+                data = data.map(async (obj) => {
+                  obj.order_vector = 'BigCommerce';
+
+                  [
+                    obj.volumeTotal,
+                    obj.singleUnits,
+                    obj.twoSixUnits,
+                    obj.eightEighteenUnits,
+                    obj.twentyFourPlusUnits,
+                    obj.tip,
+                    obj.deliverId,
+                  ] = Array(7).fill(0);
+
+                  [
+                    obj.storeNumber,
+                    obj.orderType,
+                    obj.transactionAmount,
+                    obj.openDateTime,
+                    obj.pickupReadyDateTime,
+                    obj.partialOrder,
+                    obj.completedByEmpId,
+                    obj.completedDateTime,
+                    obj.idChecked,
+                    obj.cancelledByEmpId,
+                    obj.cancelledDateTime,
+                    obj.cancelReason,
+                    obj.cancelledByCustomer,
+                    obj.cancelledByDriver,
+                    obj.requestedPickuptime,
+                    obj.ccType,
+                    obj.ccLastFourNumber,
+                    obj.customerType,
+                    obj.browserVersion,
+                    obj.deliveryType,
+                    obj.deliveryEta,
+                    obj.deliveryScheduledDateTime,
+                    obj.refunded,
+                    obj.refundReason,
+                    obj.deliverName,
+                    obj.deliveredDate,
+                    obj.deliveryAddress,
+                    obj.deliveryCity,
+                    obj.pickUpType,
+                    obj.customerDob,
+                    obj.customerSalutation,
+                  ] = Array(31).fill('');
+
+                  if (obj.billing_address.form_fields[0]) {
+                    const value = JSON.parse(
+                      obj.billing_address.form_fields[0]?.value,
+                    );
+                    obj.storeNumber = value.store_id;
+                    obj.orderType = value.order_type;
+                    obj.customerDob = value.dob;
+                    obj.customerSalutation = value.salutation;
+                  }
+
+                  obj.submittedDateTime = moment
+                    .utc(new Date(obj.date_created))
+                    .format('YYYY-MM-DD hh:mmA');
+
+                  obj.submittedDate = moment
+                    .utc(new Date(obj.date_created))
+                    .format('YYYY-MM-DD');
+
+                  obj.productTotal = obj.total_ex_tax;
+
+                  obj.deliveryFee = obj.shipping_cost_ex_tax;
+
+                  obj.deliveryFeeHst = obj.shipping_cost_tax;
+
+                  obj.grandTotal =
+                    parseFloat(obj.product_total) +
+                    parseFloat(obj.deliveryFee) +
+                    parseFloat(obj.deliveryFeeHst);
+
+                  obj.customerName = obj.billing_address.first_name.concat(
+                    obj.billing_address.last_name,
+                  );
+                  obj.customerEmail = obj.billing_address.email;
+                  obj.postalCode = obj.billing_address.zip;
+                  obj.deliveryAddress = `${obj.billing_address.street_1},${
+                    obj.billing_address.street_2
+                  }${obj.billing_address.street_2 ? ',' : ''}${
+                    obj.billing_address.city
+                  }${obj.billing_address.city ? ',' : ''}${
+                    obj.billing_address.state
+                  }${obj.billing_address.state ? ',' : ''}${
+                    obj.billing_address.zip
+                  }`;
+                  obj.deliveryCity = obj.billing_address.city;
+                  obj.deliveryPostalCode = obj.billing_address.zip;
+                  obj.productsArray = await this.getOrderProducts(
+                    obj.products.url,
+                  );
+                  obj.productsArray.filter((o) => {
+                    if (o.packSize == 1) {
+                      obj.singleUnits += o.quantity;
+                    } else if (o.packSize >= 2 && o.packSize <= 6) {
+                      obj.twoSixUnits += o.quantity;
+                    } else if (o.packSize >= 8 && o.packSize <= 18) {
+                      obj.eightEighteenUnits += o.quantity;
+                    } else if (o.packSize >= 24) {
+                      obj.twentyFourPlusUnits += o.quantity;
+                    }
+                    return;
+                  });
+                  return obj;
+                });
+                await Promise.all(data);
+                Object.assign(response?.data, { data });
                 return response.data;
               } else {
                 flag = false;
-                return orders;
+                return;
               }
             }),
             catchError((err) => {
@@ -1115,12 +1227,50 @@ export class StoreService {
           }
         });
       }
-      limitedOrders.map((order) => {
-        orders.push(order);
-      });
+      if (limitedOrders) {
+        limitedOrders.map((order) => {
+          orders.push(order);
+        });
+      }
     }
     // console.log(orders.length);
     return orders;
+  }
+
+  async getOrderProducts(url: string): Promise<any> {
+    return await lastValueFrom(
+      this.httpService
+        .get(url, {
+          headers: {
+            'x-auth-token': this.configService.get('bigcom').access_token,
+          },
+        })
+        .pipe(
+          map((res) => {
+            if (res.data && res.data.length) {
+              let productData = res.data;
+              let i = 1;
+              productData = productData.map((productObj) => {
+                productObj.line_item = i;
+                i += 1;
+
+                const productInfo =
+                  productObj.product_options[0].display_value.split('X');
+                const product = productInfo[1].trim().split(' ');
+                productObj.packSize = productInfo[0].trim() || '';
+                // productObj.volume = product[1] + product[2] || '',
+                productObj.volume = product[1] || '';
+                productObj.container = product[0] || '';
+                productObj.itemtotal = productObj.total_inc_tax;
+                return productObj;
+              });
+              Object.assign(res?.data, { productData });
+              return res.data;
+            }
+            return;
+          }),
+        ),
+    );
   }
 }
 
