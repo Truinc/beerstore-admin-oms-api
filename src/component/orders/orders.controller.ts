9be65164e0d176ff659f1b485806880c03328b78
savneetkaur21 <savneet@tru.agency>
2022-05-19 10:41:25 +0530

    shifted reports endpoint from store to Orders

--- src/component/orders/orders.controller.ts
+++ src/component/orders/orders.controller.ts
@@ -5,13 +5,18 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Post,
   Put,
   Query,
+  UseGuards,
+  HttpStatus,
 } from '@nestjs/common';
+import { ApiQuery, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { CreateOrderDto } from './dto';
 import { OrdersService } from './orders.service';
+import JwtAccessGuard from '@beerstore/core/guards/jwt-access.guard';
 
 // customer id should be 0 in case of guest order
 @Controller('orders')
@@ -74,5 +79,44 @@ export class OrdersController {
   async deleteOrder(@Param('orderId') orderId) {
     const response = await this.ordersService.deleteOrder(orderId);
     return response;
+  }
+
+  @ApiQuery({
+    name: 'status_id',
+    description:
+      ' 1 => "Pending", 2 => "In Transit (shipped)", 3 => "In Transit (Partially Shipped)", 4 => "Refunded", 5 => "Cancelled", 6 => "Returned (Declined)", 7 => "Awaiting Payment", 8 => "Awaiting Pickup", 9 => "Awaiting Delivery (Awaiting Shipment)", 10 => "Completed", 11 => "Awaiting Fulfillment", 12 => "Manual Verification Required", 13 => "Disputed", 14 => "Partially Refunded"',
+    required: false,
+  })
+  @ApiQuery({
+    name: 'store_id',
+    description: 'store id of store i.e. 12321',
+    required: false,
+  })
+  @ApiQuery({
+    name: 'min_date_created',
+    required: false,
+    description: 'Minimum date the order was created i.e. 2021-04-20',
+  })
+  @ApiQuery({
+    name: 'max_date_created',
+    required: false,
+    description: 'Maximum date the order was created i.e. 2022-04-20',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized Response' })
+  @UseGuards(JwtAccessGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('/report/getAllOrders')
+  async getAllOrders(
+    @Query('status_id') status_id: number,
+    @Query('store_id') store_id: number,
+    @Query('min_date_created') min_date_created: Date,
+    @Query('max_date_created') max_date_created: Date,
+  ) {
+    return this.ordersService.getAllOrdersReports(
+      status_id,
+      store_id,
+      min_date_created,
+      max_date_created,
+    );
   }
 }
--- src/component/orders/orders.service.ts
+++ src/component/orders/orders.service.ts
@@ -4,6 +4,7 @@ import {
   Injectable,
 } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 import { catchError, lastValueFrom, map, Observable } from 'rxjs';
 import { ApiType, ApiVersion } from '@beerstore/core/interfaces/urls';
 import {
@@ -12,11 +13,15 @@ import {
   handleError,
   serviceHeader,
 } from '@beerstore/core/utils';
+import * as moment from 'moment';
 import { CreateOrderDto } from './dto';
 import { Order, OrderQuery } from '@beerstore/core/interfaces';
 @Injectable()
 export class OrdersService {
-  constructor(private httpService: HttpService) {}
+  constructor(
+    private httpService: HttpService,
+    private configService: ConfigService,
+  ) {}
 
   async getOrder(orderId: string): Promise<any> {
     const queryStr = `/${orderId}`;
@@ -114,6 +119,226 @@ export class OrdersService {
     return this.httpService.get(url, serviceHeader()).pipe(
       map((response) => response.data),
       catchError(handleError<any>(null)),
+    );
+  }
+
+  async getAllOrdersReports(
+    status_id: number,
+    store_id: number,
+    min_date_created: Date,
+    max_date_created: Date,
+  ): Promise<any> {
+    let filter = ``;
+    const uri = `v2/orders`;
+    const orders = [];
+    let flag = true;
+    let page = 1;
+
+    if (status_id) {
+      filter = `${filter}&status_id=${status_id}`;
+    }
+    if (min_date_created) {
+      filter = `${filter}&min_date_created=${moment(min_date_created).format(
+        'YYYY-MM-DD',
+      )}`;
+    }
+    if (max_date_created) {
+      filter = `${filter}&max_date_created=${moment(max_date_created).format(
+        'YYYY-MM-DD',
+      )}`;
+    }
+
+    while (flag == true) {
+      let limitedOrders = await lastValueFrom(
+        this.httpService
+          .get(
+            `${this.configService.get('bigcom').url}/stores/${
+              this.configService.get('bigcom').store
+            }/${uri}?limit=250&page=${page}&${filter}`,
+            {
+              headers: {
+                'x-auth-token': this.configService.get('bigcom').access_token,
+              },
+            },
+          )
+          .pipe(
+            map(async (response) => {
+              if (response.data && response.data.length) {
+                page += 1;
+                let data = response.data;
+                data = data.map(async (obj) => {
+                  obj.order_vector = 'BigCommerce';
+
+                  [
+                    obj.volumeTotal,
+                    obj.singleUnits,
+                    obj.twoSixUnits,
+                    obj.eightEighteenUnits,
+                    obj.twentyFourPlusUnits,
+                    obj.tip,
+                    obj.deliverId,
+                  ] = Array(7).fill(0);
+
+                  [
+                    obj.storeNumber,
+                    obj.orderType,
+                    obj.transactionAmount,
+                    obj.openDateTime,
+                    obj.pickupReadyDateTime,
+                    obj.partialOrder,
+                    obj.completedByEmpId,
+                    obj.completedDateTime,
+                    obj.idChecked,
+                    obj.cancelledByEmpId,
+                    obj.cancelledDateTime,
+                    obj.cancelReason,
+                    obj.cancelledByCustomer,
+                    obj.cancelledByDriver,
+                    obj.requestedPickuptime,
+                    obj.ccType,
+                    obj.ccLastFourNumber,
+                    obj.customerType,
+                    obj.browserVersion,
+                    obj.deliveryType,
+                    obj.deliveryEta,
+                    obj.deliveryScheduledDateTime,
+                    obj.refunded,
+                    obj.refundReason,
+                    obj.deliverName,
+                    obj.deliveredDate,
+                    obj.deliveryAddress,
+                    obj.deliveryCity,
+                    obj.pickUpType,
+                    obj.customerDob,
+                    obj.customerSalutation,
+                  ] = Array(31).fill('');
+
+                  if (obj.billing_address.form_fields[0]) {
+                    const value = JSON.parse(
+                      obj.billing_address.form_fields[0]?.value,
+                    );
+                    obj.storeNumber = value.store_id;
+                    obj.orderType = value.order_type;
+                    obj.customerDob = value.dob;
+                    obj.customerSalutation = value.salutation;
+                  }
+
+                  obj.submittedDateTime = moment
+                    .utc(new Date(obj.date_created))
+                    .format('YYYY-MM-DD hh:mmA');
+
+                  obj.submittedDate = moment
+                    .utc(new Date(obj.date_created))
+                    .format('YYYY-MM-DD');
+
+                  obj.productTotal = obj.total_ex_tax;
+
+                  obj.deliveryFee = obj.shipping_cost_ex_tax;
+
+                  obj.deliveryFeeHst = obj.shipping_cost_tax;
+
+                  obj.grandTotal =
+                    parseFloat(obj.product_total) +
+                    parseFloat(obj.deliveryFee) +
+                    parseFloat(obj.deliveryFeeHst);
+
+                  obj.customerName = obj.billing_address.first_name.concat(
+                    obj.billing_address.last_name,
+                  );
+                  obj.customerEmail = obj.billing_address.email;
+                  obj.postalCode = obj.billing_address.zip;
+                  obj.deliveryAddress = `${obj.billing_address.street_1},${
+                    obj.billing_address.street_2
+                  }${obj.billing_address.street_2 ? ',' : ''}${
+                    obj.billing_address.city
+                  }${obj.billing_address.city ? ',' : ''}${
+                    obj.billing_address.state
+                  }${obj.billing_address.state ? ',' : ''}${
+                    obj.billing_address.zip
+                  }`;
+                  obj.deliveryCity = obj.billing_address.city;
+                  obj.deliveryPostalCode = obj.billing_address.zip;
+                  obj.productsArray = await this.getOrderProductsReports(
+                    obj.products.url,
+                  );
+                  obj.productsArray.filter((o) => {
+                    if (o.packSize == 1) {
+                      obj.singleUnits += o.quantity;
+                    } else if (o.packSize >= 2 && o.packSize <= 6) {
+                      obj.twoSixUnits += o.quantity;
+                    } else if (o.packSize >= 8 && o.packSize <= 18) {
+                      obj.eightEighteenUnits += o.quantity;
+                    } else if (o.packSize >= 24) {
+                      obj.twentyFourPlusUnits += o.quantity;
+                    }
+                    return;
+                  });
+                  return obj;
+                });
+                await Promise.all(data);
+                Object.assign(response?.data, { data });
+                return response.data;
+              } else {
+                flag = false;
+                return;
+              }
+            }),
+            catchError((err) => {
+              throw new BadGatewayException(err.message);
+            }),
+          ),
+      );
+      if (store_id) {
+        limitedOrders = limitedOrders.filter((e) => {
+          const value = JSON.parse(e.billing_address.form_fields[0].value);
+          if (value.store_id == store_id) {
+            return e;
+          }
+        });
+      }
+      if (limitedOrders) {
+        limitedOrders.map((order) => {
+          orders.push(order);
+        });
+      }
+    }
+    // console.log(orders.length);
+    return orders;
+  }
+
+  async getOrderProductsReports(url: string): Promise<any> {
+    return await lastValueFrom(
+      this.httpService
+        .get(url, {
+          headers: {
+            'x-auth-token': this.configService.get('bigcom').access_token,
+          },
+        })
+        .pipe(
+          map((res) => {
+            if (res.data && res.data.length) {
+              let productData = res.data;
+              let i = 1;
+              productData = productData.map((productObj) => {
+                productObj.line_item = i;
+                i += 1;
+
+                const productInfo =
+                  productObj.product_options[0].display_value.split('X');
+                const product = productInfo[1].trim().split(' ');
+                productObj.packSize = productInfo[0].trim() || '';
+                // productObj.volume = product[1] + product[2] || '',
+                productObj.volume = product[1] || '';
+                productObj.container = product[0] || '';
+                productObj.itemtotal = productObj.total_inc_tax;
+                return productObj;
+              });
+              Object.assign(res?.data, { productData });
+              return res.data;
+            }
+            return;
+          }),
+        ),
     );
   }
 }
